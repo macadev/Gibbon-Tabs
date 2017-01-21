fa0e14fb9e29920b1c3fa916fa77bd5b1bca8cec
macadev <danielmacario5@gmail.com>
2017-01-21 16:44:12 -0500

    add basic extension to display tab urls in popup

---
+++ popup.js
@@ -0,0 +1,23 @@
+function getAllTabs(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {};
+  chrome.tabs.query(queryInfo, function(tabs) {
+    callback(tabs);
+  });
+}
+
+function renderStatus(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("getting all tabs");
+  getAllTabs(function(tabs) {
+    var urls = "";
+    for (let tab of tabs) {
+      urls = urls + tab.url + "\n";
+    }
+    renderStatus(urls);
+  });
+});
