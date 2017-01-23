db4e1043b8d7f69618d8a90c178681c9f07913a7
macadev <danielmacario5@gmail.com>
2017-01-22 22:27:12 -0500

    fix indentation of background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 chrome.commands.onCommand.addListener(function(command) {
-	if(command === "show_tabs"){
+  if(command === "show_tabs"){
 		console.log("Show tabs command called");
 		var createData = {
 			url: chrome.extension.getURL("popup.html"),
 			type: "popup"
 		}
 		chrome.windows.create(createData);
-    }
+  }
 });
