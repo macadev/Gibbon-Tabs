cae890fa8ef08ed0d4dbafd0befa94166f89210f
Daniel Macario <danielmacario5@gmail.com>
2017-01-24 20:45:24 -0500

    add fully working tab selection

--- popup.js
+++ popup.js
@@ -1,10 +1,53 @@
-// Close the popup when the escape key is pressed
 document.onkeydown = function(event) {
   event = event || window.event;
+  // Escape key
   if (event.keyCode == 27) {
     window.close();
   }
+  // Down arrow key
+  if (event.keyCode == 40) {
+    removeHighlight(highlightIndex);
+    if (highlightIndex < numTabs) ++highlightIndex;
+    highlightTab(highlightIndex);
+    return;
+  }
+  // Up arrow key
+  if (event.keyCode == 38) {
+    removeHighlight(highlightIndex);
+    if (highlightIndex > 1) --highlightIndex;
+    highlightTab(highlightIndex);
+    return;
+  }
+
+  // Enter key
+  if (event.keyCode == 13) {
+    activateTab(highlightIndex);
+    window.close();
+  }
+
 };
+
+function activateTab(tabIndex) {
+  var tabId = tabsToRender[tabIndex - 1].id;
+  chrome.tabs.update(tabId, {
+    active: true,
+    highlighted: true
+  });
+}
+
+function removeHighlight(tabIndex) {
+  var active = document.getElementById("search_id_" + tabIndex);
+  if (active !== null) {
+    active.classList.remove("highlighted");
+  }
+}
+
+function highlightTab(tabIndex) {
+  var toHighlight = document.getElementById("search_id_" + tabIndex);
+  if (toHighlight !== null) {
+    toHighlight.classList.add("highlighted");
+  }
+}
 
 function getAllTabs(callback) {
   var queryInfo = {};
@@ -13,13 +56,18 @@ function getAllTabs(callback) {
   });
 }
 
-function createTabHtmlElement(tabData) {
+function createTabHtmlElement(tabData, tabIndex) {
   // TODO: embedding html like this is horrible. Fix.
-  return "<div id=\"tab\"><div>" + tabData.title + "</div><div>" + tabData.url +"</div></div>";
+  return "<div class=\"tab\" id=\"search_id_" + tabIndex + "\"><div>" + tabData.title + "</div><div>" + tabData.url +"</div></div>";
 }
 
 function renderSearchResults(tabsToRender) {
-  document.getElementById('tab_container').innerHTML = tabsToRender.join('');
+  var tabsHtml = "";
+  for (let tab of tabsToRender) {
+    tabsHtml += tab.html;
+  }
+  numTabs = tabsToRender.length
+  document.getElementById('tab_container').innerHTML = tabsHtml;
 }
 
 function searchTabs() {
@@ -32,27 +80,38 @@ function searchTabs() {
     results = fuse.search(searchText);
   }
 
-  var tabsToRender = [];
+  tabsToRender = [];
+  var tabIndex = 1;
   for (let result of results) {
-    tabsToRender.push(createTabHtmlElement(result));
+    tabsToRender.push({
+      html: createTabHtmlElement(result, tabIndex),
+      id: result.id
+    });
+    tabIndex++;
   }
   renderSearchResults(tabsToRender);
+  highlightIndex = 1; // Reset highlight index to the first tab
+  if (tabsToRender.length > 0) highlightTab(highlightIndex); // highlight first result
 }
 
 var fuse; // used to perform the fuzzy search
 var tabsToSearch = [];
+var tabsToRender = [];
+var highlightIndex = 1;
+var numTabs;
 document.addEventListener('DOMContentLoaded', function() {
   // Add event handler to input box
   var inputBox = document.getElementById('search_box');
   inputBox.focus();
-  inputBox.addEventListener('keyup', searchTabs);
+  inputBox.oninput = searchTabs;
 
   getAllTabs(function(tabs) {
     for (let tab of tabs) {
       tabsToSearch.push({
         title: tab.title,
-        url: tab.url
-      })
+        url: tab.url,
+        id: tab.id
+      });
     }
 
     var searchOpts = {
