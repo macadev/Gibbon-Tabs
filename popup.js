b36bebf0e30f8bb9677e0ed20472584e7190b37a
macadev <danielmacario5@gmail.com>
2017-02-21 18:49:20 -0500

    add fully functional arrowkey + mouse navigation

--- popup.js
+++ popup.js
@@ -47,12 +47,18 @@ function removeHighlight(tabIndex) {
   }
 }
 
+function isScrolledIntoView(el) {
+  var elemTop = el.getBoundingClientRect().top;
+  var elemBottom = el.getBoundingClientRect().bottom;
+  return (elemTop >= 0) && (elemBottom <= window.innerHeight);
+}
+
 var TAB_BORDER_COLORS = ["#568AF2", "#DE5259", "#1AA15F", "#FFCE45"];
 function highlightTab(tabIndex, shouldScrollIntoView) {
   var toHighlight = document.getElementById("search_id_" + tabIndex);
   if (toHighlight !== null) {
     toHighlight.classList.add("highlighted");
-    if (shouldScrollIntoView) toHighlight.scrollIntoView(false);
+    if (shouldScrollIntoView && !isScrolledIntoView(toHighlight)) toHighlight.scrollIntoView(false);
     toHighlight.style["border-left-color"] = TAB_BORDER_COLORS[tabIndex % 4];
   }
 }
