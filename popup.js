6449927b25a59c9e80b8df694ac5cbfc63668b9f
macadev <danielmacario5@gmail.com>
2017-03-04 16:58:47 -0500

    add awesome save snap menu, remove sweet alert stuff

--- popup.js
+++ popup.js
@@ -86,39 +86,49 @@ function getAllTabs(callback) {
   });
 }
 
-function saveTabsSnapshot() {
-  swal({
-    title: "Create tabs Snapshot!",
-    type: "input",
-    showCancelButton: true,
-    closeOnConfirm: false,
-    animation: "slide-from-top",
-    inputPlaceholder: "Snapshot name"
-  },
-  function(inputValue){
-    if (inputValue === false) return false;
+function showSaveSnapshotMenu() {
+  var saveSnapButtonRect = document.getElementById('save_snap_button').getBoundingClientRect();
+  var saveSnapMenu = document.getElementById("save_snap_menu");
+  if (saveSnapMenu.style.display == "initial") {
+    saveSnapMenu.style.display = "none";
+    return;
+  }
 
-    if (inputValue === "") {
-      swal.showInputError("You need to write something!");
-      return false
-    }
+  document.getElementById('save_snap_name_input').value = "";
+  saveSnapMenu.style.left = saveSnapButtonRect.left + "px";
+  saveSnapMenu.style.top = saveSnapButtonRect.bottom + "px";
+  saveSnapMenu.style.display = "initial";
+}
 
-    getTabsSnapshots(function(tabSnapsObj) {
-      getAllTabs(function (tabs) {
-        var newSnapshot = {
-          name: inputValue,
-          tabs: tabs
-        };
-
-        if (tabSnapsObj.tabSnaps === undefined) {
-          tabSnapsObj.tabSnaps = { listOfSnaps: [] };
-        }
-
-        tabSnapsObj.tabSnaps.listOfSnaps.push(newSnapshot);
-        chrome.storage.local.set({ "tabSnaps": tabSnapsObj.tabSnaps }, function() {
-          swal("Success!", "Sanpshot \"" + inputValue + "\" has been saved.", "success");
-        });
-        swal.showInputError("Failed to save snapshot!");
+function saveSnapshot() {
+  var snapshotName = document.getElementById('save_snap_name_input').value;
+  var saveSnapshotButton = document.getElementById('submit_save_snap_button');
+  if (snapshotName == "") {
+    alert("Please specify a name for the snapshot.");
+    return;
+  }
+  getTabsSnapshots(function(tabSnapsObj) {
+    getAllTabs(function (tabs) {
+      var newSnapshot = {
+        name: snapshotName,
+        tabs: tabs
+      };
+      if (tabSnapsObj.tabSnaps === undefined) {
+        tabSnapsObj.tabSnaps = { listOfSnaps: [] };
+      }
+      tabSnapsObj.tabSnaps.listOfSnaps.push(newSnapshot);
+      chrome.storage.local.set({ "tabSnaps": tabSnapsObj.tabSnaps }, function() {
+        var originalBackground = saveSnapshotButton.style.background;
+        var originalText = saveSnapshotButton.value;
+        var saveSnapshotMenu = document.getElementById('save_snap_menu');
+        saveSnapshotButton.style.background = "#69B578";
+        saveSnapshotButton.innerText = "Success!";
+        setTimeout(function() {
+          saveSnapshotButton.style.background = originalBackground;
+          saveSnapshotButton.innerText = originalText;
+          hideElement(saveSnapshotMenu);
+        }, 1000);
+        console.log("Save successfully!");
       });
     });
   });
@@ -135,7 +145,7 @@ function renderListOfSnapshots() {
   getTabsSnapshots(function(tabSnapsObj) {
     var tabSnapsHtml = "<div id=\"tab_snap_container\">";
     if (tabSnapsObj.tabSnaps !== undefined) {
-      tabSnapsHtml += "<p id=\"snaps_exist_message\">Tab Snapshots</p>"
+      tabSnapsHtml += "<p class=\"snap_action_title\">Tab Snapshots</p>"
       for (let tabSnap of tabSnapsObj.tabSnaps.listOfSnaps) {
         tabSnapsHtml += "<div class=\"tab_snap_box\">" + tabSnap.name + "</div>";
       }
@@ -147,7 +157,7 @@ function renderListOfSnapshots() {
     tabSnapsDropdown.style.left = getSnapsButtonRect.left + "px";
     tabSnapsDropdown.style.top = getSnapsButtonRect.bottom + "px";
     tabSnapsDropdown.style.display = "initial";
-    tabSnapsDropdown.onmouseleave = hideTabSnapsDropdown.bind(null, tabSnapsDropdown);
+    tabSnapsDropdown.onmouseleave = hideElement.bind(null, tabSnapsDropdown);
     document.getElementById('tab_snaps_dropdown').innerHTML = tabSnapsHtml;
 
     var tabSnapBoxes = document.getElementsByClassName('tab_snap_box');
@@ -157,8 +167,8 @@ function renderListOfSnapshots() {
   });
 }
 
-function hideTabSnapsDropdown(dropdownElement) {
-  dropdownElement.style.display = "none";
+function hideElement(element) {
+  element.style.display = "none";
 }
 
 function activateTabSnapshot(tabData) {
@@ -300,8 +310,15 @@ document.addEventListener('DOMContentLoaded', function() {
   inputBox.focus();
   inputBox.oninput = searchTabs;
 
-  var saveSnapshotButton = document.getElementById('save_snap_button');
-  saveSnapshotButton.onclick = saveTabsSnapshot;
+  var saveSnapMenuElement = document.getElementById('save_snap_menu');
+  var showSaveSnapshotMenuButton = document.getElementById('save_snap_button');
+  showSaveSnapshotMenuButton.onclick = showSaveSnapshotMenu;
+
+  var submitSaveSnapshotButton = document.getElementById('submit_save_snap_button');
+  submitSaveSnapshotButton.onclick = saveSnapshot
+
+  var cancelSaveSnapshotButton = document.getElementById('cancel_save_snap_button');
+  cancelSaveSnapshotButton.onclick = hideElement.bind(null, saveSnapMenuElement);
 
   var renderSnapsListButton = document.getElementById('get_snaps_button');
   renderSnapsListButton.onclick = renderListOfSnapshots;
