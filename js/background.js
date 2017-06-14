5fa8ff04f0f714b00c0e51556a965bb0ecc1848a
macadev <danielmacario5@gmail.com>
2017-06-13 21:31:57 -0400

    remove useless command

--- js/background.js
+++ js/background.js
@@ -1,9 +1,1 @@
-chrome.commands.onCommand.addListener(function(command) {
-  if (command === "activate_in_popup") {
-    var createData = {
-      url: chrome.extension.getURL("popup.html"),
-      type: "popup"
-    }
-    chrome.windows.create(createData);
-  }
-});
+chrome.commands.onCommand.addListener(function(command) {});
